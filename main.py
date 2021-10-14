b443ccd860420dfa87026ad9dcd07b273a6f28bd
Harsh Rana <hr23232323@gmail.com>
2021-10-13 22:34:37 -0400

    added a buffered read method

--- main.py
+++ main.py
@@ -19,12 +19,28 @@ def read(file):
             break
         yield chunk
 
+# Helpful SO article: https://stackoverflow.com/questions/22170095/get-words-from-large-file-using-low-memory-in-python
+def buffered_read(file):
+    buffer = ''
+    block_size = 512
+    while True:
+        chunk = file.read(block_size)
+        if not chunk:
+            break
+        words = re.split("\W+", buffer + chunk)
+        buffer = words.pop()  # partial word at end of chunk or empty
+        for word in words:
+            yield word
+
+    if buffer:
+        yield buffer
+
 def word_count(input_file):
     # cache
     word_count = {}
     
     # Stream input
-    for block in read(input_file):
+    for block in buffered_read(input_file):
         block = process_text(block)
         wc = Counter(block.split())
         for w, c in wc.items():
@@ -36,18 +52,8 @@ def word_count(input_file):
     return word_count
 
 # helper to process/clean text.
+# This can be expanded to clean up input data as required. 
 def process_text(word_block):
     word_block = word_block.lower()
     return re.sub('[^\w]', ' ', word_block)
-    
-    
-#print(re.sub("[^\w]", ' ', "this is a string.[123]"))
-
-    
-#print(word_count(input_file))
-#print(Counter("this is a a A sentence".split()))
-
-#print({'key1': 'value1'}.update({'key2': 'value2'}))
-
-#print(get_word_count_from_block("this is a a A sentence"))
 
